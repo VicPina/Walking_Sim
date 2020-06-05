cb789fad98b1dcb33d27b424f8dd8ae95ef2dc97
VicPina <vicpanzu7@gmail.com>
2020-06-04 22:49:37 -0500

    Definitive Furniture Prefabs

---
+++ Walking Simulator/Assets/Scripts/PlayerController1.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController1 : MonoBehaviour
+{
+    public float speed = 5;
+    public float rotationSpeed = 100;
+       
+    private Vector3 movement = Vector3.zero;
+    private Vector3 rotation = Vector3.zero;
+
+    private CharacterController chController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        chController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+    }
+    public void FixedUpdate()
+    {
+        chController.Move(movement * Time.deltaTime * speed);
+    }
+}
