27fe317f84012679aac53715b2f4d63fc6f02c20
VicPina <vicpanzu7@gmail.com>
2020-06-05 08:59:47 -0500

    Raycast testing

---
+++ Walking Simulator/Assets/Scripts/Raycast.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Raycast : MonoBehaviour 
+{
+    public LayerMask mask;
+    public Camera fpv;
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ray = fpv.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hitInfo;
+        if (Physics.Raycast(ray, out hitInfo, 100, mask)) 
+        {
+            print(hitInfo.collider.gameObject.GetComponent("objectManager").name);
+            Debug.DrawLine(ray.origin, hitInfo.point, Color.yellow);
+            if(Input.GetMouseButtonDown(0)) { Destroy(hitInfo.transform.gameObject); }
+        }
+        else { Debug.DrawLine(ray.origin, ray.origin + ray.direction * 100, Color.white); }
+    }
+}
---
+++ Walking Simulator/Assets/Scripts/objectManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class objectManager : MonoBehaviour
+{
+    public string itemName;
+    public string itemDialogue;
+
+    public string GetName() { return itemName; }
+    public string GetDialogue() { return itemDialogue; }
+}
